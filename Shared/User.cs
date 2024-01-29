41764adbce3e3479cfde11f7e06fc30576a55c09
Stefan Atanasoski <stefmefatanas@gmail.com>
2024-01-29 05:24:39 +0000

    Checkout & validation

--- Shared/User.cs
+++ Shared/User.cs
@@ -13,7 +13,7 @@ namespace Handball_Shopv1.Shared
         public string Password { get; set; }
         public string UserName { get; set; } = string.Empty;
         public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? Surname { get; set; }
 
     }
 }
